bff491c1c30ae3ac39dedd6a5b59d658b90dd74c
Jayla Nickens <jaylanickensgill11@gmail.com>
2020-07-14 16:57:20 +0000

    .

---
+++ main.py
@@ -0,0 +1,17 @@
+a = "Jayla Nickens- Gill"
+
+b = "My favorite artist is Chris Brown"
+
+c = "My favorite song is selfish"
+
+d = "A place I would like to go is Hawaii"
+
+e = "A hobby I have is baking or playing games with friends"
+f ="7/14/2020"
+
+print (a)
+print (b)
+print (c)
+print (d)
+print (e)
+print (f)
